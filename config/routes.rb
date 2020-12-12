db398a6084ec49c82a99f7612026ee6b18c35ca9
Jesse Spevack <jspevack@gmail.com>
2020-12-12 08:57:23 -0700

    standard and fix (#28)

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root 'welcome#index'
+  root "welcome#index"
 
-  get 'auth/slack/callback', to: 'sessions#create'
-  delete 'logout', to: 'sessions#destroy'
+  get "auth/slack/callback", to: "sessions#create"
+  delete "logout", to: "sessions#destroy"
 end
