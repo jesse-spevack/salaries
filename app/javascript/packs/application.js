72df09a91cbd55b0eeb58f6088fec5fde2f1311a
PlanetEfficacy <jesse@mountainmantechnologies.com>
2021-01-01 12:21:13 -0700

    Another date picker commit

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,3 +15,13 @@ ActiveStorage.start()
 import "controllers"
 
 require("stylesheets/application.scss")
+
+import { Application } from "stimulus"
+import { definitionsFromContext } from "stimulus/webpack-helpers"
+const application = Application.start()
+const context = require.context("../controllers", true, /\.js$/)
+application.load(definitionsFromContext(context))
+
+import Flatpickr from 'stimulus-flatpickr'
+require("flatpickr/dist/flatpickr.css")
+application.register("flatpickr", Flatpickr)
