8d0ab956b66622cddbffaff233bc86a4672c73da
PlanetEfficacy <jesse@mountainmantechnologies.com>
2021-01-24 08:04:10 -0700

    Add edit and delete capabilities

--- app/components/table_data_link_component.rb
+++ app/components/table_data_link_component.rb
@@ -1,8 +1,9 @@
 # frozen_string_literal: true
 
 class TableDataLinkComponent < ViewComponent::Base
-  def initialize(text:, path:)
+  def initialize(text:, path:, data: {})
     @text = text
     @path = path
+    @data = data
   end
 end
