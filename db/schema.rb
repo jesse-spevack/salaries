913dcfca047630d7ae583624187f753103c7e40d
PlanetEfficacy <jesse@mountainmantechnologies.com>
2021-01-24 20:21:01 -0700

    Add unique index

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_25_031233) do
+ActiveRecord::Schema.define(version: 2021_01_25_031728) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2021_01_25_031233) do
     t.integer "graduation_year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_profiles_on_user_id", unique: true
   end
 
   create_table "salaries", force: :cascade do |t|
