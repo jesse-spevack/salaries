1a1130f67c5b608aa81d5ff516688a35d020d3e1
PlanetEfficacy <jesse@mountainmantechnologies.com>
2021-01-27 18:58:58 -0700

    uncomment

--- spec/components/previews/test_component_preview.rb
+++ spec/components/previews/test_component_preview.rb
@@ -8,14 +8,14 @@ class TestComponentPreview < ViewComponent::Preview
   end
 
   def with_long_title
-    # render(TestComponent.new(title: "This is a really long title to see how the component renders this"))
+    render(TestComponent.new(title: "This is a really long title to see how the component renders this"))
   end
 
   def with_content_block
-  #   render(TestComponent.new(title: "This component accepts a block of content")) do
-  #     tag.div do
-  #       content_tag(:span, "Hello")
-  #     end
-  #   end
+    render(TestComponent.new(title: "This component accepts a block of content")) do
+      tag.div do
+        content_tag(:span, "Hello")
+      end
+    end
   end
 end
