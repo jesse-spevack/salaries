3263d453b890e9ef4b8426853985adee19a0ec9d
PlanetEfficacy <jesse@mountainmantechnologies.com>
2021-01-30 09:29:08 -0700

    replace figaro in omniauth

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,9 +3,9 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider(
     :slack,
-    Figaro.env.turing_slack_client_id,
-    Figaro.env.turing_slack_client_secret,
+    Rails.application.credentials[:turing_slack_client_id],
+    Rails.application.credentials[:turing_slack_client_secret],
     scope: "users:read",
-    team: Figaro.env.turing_slack_team_id
+    team: Rails.application.credentials[:turing_slack_team_id]
   )
 end
